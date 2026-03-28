52481eafd6018a2bc260b29731059d197837e26c
Chloe Prime <chloe_von_einzbern@qq.com>
2026-03-29 03:32:02 +0800

    调整达姆弹的设定：老式空尖弹 -> SST 聚合物扩张弹并修改配方

--- src/main/java/cn/chloeprime/gunsmithlib_std_ammo/common/effect/DumHurtEffect.java
+++ src/main/java/cn/chloeprime/gunsmithlib_std_ammo/common/effect/DumHurtEffect.java
@@ -8,13 +8,19 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import javax.annotation.Nonnull;
 
 public class DumHurtEffect extends MobEffectBaseUtility {
+
+    public static final String MODIFIER_ID = "0be83417-383c-4892-84b7-b1aa46180ce6";
+
     public DumHurtEffect(MobEffectCategory category, int color) {
         super(category, color);
     }
 
     public static MobEffect bootstrap() {
         return new DumHurtEffect(MobEffectCategory.HARMFUL, 0x2e3a46)
-                .addAttributeModifier(Attributes.MAX_HEALTH, "0be83417-383c-4892-84b7-b1aa46180ce6", -0.05, AttributeModifier.Operation.MULTIPLY_TOTAL);
+                .addAttributeModifier(Attributes.MAX_HEALTH, MODIFIER_ID, -0.05, AttributeModifier.Operation.MULTIPLY_TOTAL)
+                .addAttributeModifier(Attributes.MOVEMENT_SPEED, MODIFIER_ID, -0.05, AttributeModifier.Operation.MULTIPLY_TOTAL)
+                .addAttributeModifier(Attributes.FLYING_SPEED, MODIFIER_ID, -0.05, AttributeModifier.Operation.MULTIPLY_TOTAL)
+                .addAttributeModifier(Attributes.ATTACK_SPEED, MODIFIER_ID, -0.05, AttributeModifier.Operation.MULTIPLY_TOTAL);
     }
 
     @Override
