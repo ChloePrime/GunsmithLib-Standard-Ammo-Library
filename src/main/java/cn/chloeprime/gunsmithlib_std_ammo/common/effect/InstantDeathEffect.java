7b03947b7acbcf19a83fa74cef9ac476f2e986b5
Chloe Prime <chloe_von_einzbern@qq.com>
2026-04-12 03:56:32 +0800

    让即死效果对 boss 无效

--- src/main/java/cn/chloeprime/gunsmithlib_std_ammo/common/effect/InstantDeathEffect.java
+++ src/main/java/cn/chloeprime/gunsmithlib_std_ammo/common/effect/InstantDeathEffect.java
@@ -5,14 +5,20 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.InstantenousMobEffect;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.util.FakePlayerFactory;
 
 import javax.annotation.Nonnull;
 import java.util.UUID;
 
 public class InstantDeathEffect extends InstantenousMobEffect {
+    public static boolean isBoss(Entity entity) {
+        return entity.getType().is(Tags.EntityTypes.BOSSES);
+    }
+
     private static final GameProfile INVULNERABILITY_TESTER_PROFILE = new GameProfile(
             UUID.fromString("9dfe73d1-d877-43c9-b528-4c95630fcf15"),
             "[Invulnerability Tester]");
@@ -30,6 +36,8 @@ public class InstantDeathEffect extends InstantenousMobEffect {
             if (player.isCreative() || player.getAbilities().invulnerable) {
                 return;
             }
+        } else if (isBoss(victim)) {
+            return;
         } else if (victim.level() instanceof ServerLevel serverLevel) {
             var tester = FakePlayerFactory.get(serverLevel, INVULNERABILITY_TESTER_PROFILE);
             if (victim.isInvulnerableTo(victim.damageSources().playerAttack(tester))) {
